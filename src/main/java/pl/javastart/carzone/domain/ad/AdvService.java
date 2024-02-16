c96a6ccd4ff082877bf828f4736c2e61404de88d
1 <daniel.galwa@gmail.com>
2024-02-16 17:18:00 +0100

    version 1

--- src/main/java/pl/javastart/carzone/domain/ad/AdvService.java
+++ src/main/java/pl/javastart/carzone/domain/ad/AdvService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import pl.javastart.carzone.domain.ad.dto.AdDto;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdvService {
@@ -17,5 +18,8 @@ public class AdvService {
         return adRepository.findAll().stream()
                 .map(AdDtoMapper::map)
                 .toList();
+    }
+    public Optional<AdDto> findAdById(long id){
+        return adRepository.findById(id).map(AdDtoMapper::map);
     }
 }
--- src/main/java/pl/javastart/carzone/web/AdvertController.java
+++ src/main/java/pl/javastart/carzone/web/AdvertController.java
@@ -1,13 +1,28 @@
 package pl.javastart.carzone.web;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
+import pl.javastart.carzone.domain.ad.AdvService;
+import pl.javastart.carzone.domain.ad.dto.AdDto;
+
+import java.util.Optional;
 
 @Controller
 public class AdvertController {
+    private final AdvService advService;
 
-    @GetMapping("/test")
-    String advert(Model model) {
-        return "advertv2";
+    public AdvertController(AdvService advService) {
+        this.advService = advService;
+    }
+
+    @GetMapping("/ad/{id}")
+    String getAdvert(@PathVariable long id, Model model) {
+        AdDto ad = advService.findAdById(id).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        model.addAttribute("ad", ad);
+        return "advert";
     }
 }
